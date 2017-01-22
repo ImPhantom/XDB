e243833c56adfcf68065398ad60912dced123e1c
ImPhantom <danieljcannon@live.com>
2017-01-22 02:43:03 -0500

    Crude "Server Info" command

--- src/XDB/Modules/Chat.cs
+++ src/XDB/Modules/Chat.cs
@@ -12,8 +12,81 @@ namespace XDB.Modules
 {
     public class Chat : ModuleBase
     {
+        [Command("serverinfo")]
+        [Remarks("Display's the current guild's information.")]
+        public async Task ServerInfo()
+        {
+            // Ignore this shit
+            var owner = await Context.Guild.GetOwnerAsync();
+            var users = await Context.Guild.GetUsersAsync();
+            var text = await Context.Guild.GetTextChannelsAsync();
+            var voice = await Context.Guild.GetVoiceChannelsAsync();
+
+            var date = $"{Context.Guild.CreatedAt.Month}/{Context.Guild.CreatedAt.Day}/{Context.Guild.CreatedAt.Year}";
+            var footer = new EmbedFooterBuilder()
+            {
+                IconUrl = Context.Guild.IconUrl,
+                Text = $"Server ID: {Context.Guild.Id}"
+            };
+            var embed = new EmbedBuilder()
+            {
+                Color = new Color(29, 140, 209),
+                ThumbnailUrl = Context.Guild.IconUrl,
+                Footer = footer
+            };
+            embed.AddField(x =>
+            {
+                x.Name = "Owner:";
+                x.Value = owner.Mention;
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Server Name:";
+                x.Value = $"{Context.Guild.Name}";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Voice Region:";
+                x.Value = $"`{Context.Guild.VoiceRegionId}`";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Created:";
+                x.Value = $"{date}";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Roles:";
+                x.Value = $"{Context.Guild.Roles.Count}";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Users:";
+                x.Value = $"{users.Count}";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Text Channels:";
+                x.Value = $"{text.Count()}";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Voice Channels:";
+                x.Value = $"{voice.Count()}";
+                x.IsInline = true;
+            });
+            await ReplyAsync("", false, embed.Build());
+        }
+        
         [Command("info")]
-        [Remarks("Displays the bots information and statistics.")]
+        [Remarks("Display's the bots information and statistics.")]
         public async Task Info()
         {
             var application = await Context.Client.GetApplicationInfoAsync();
