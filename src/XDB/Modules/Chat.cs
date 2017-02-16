74f3267c5aab1d91597ff1a3efc4b0a96d629201
ImPhantom <danieljcannon@live.com>
2017-02-16 00:52:55 -0500

    Moved ~info to SelfModule

--- src/XDB/Modules/Chat.cs
+++ src/XDB/Modules/Chat.cs
@@ -2,12 +2,7 @@
 using Discord.Commands;
 using Discord.WebSocket;
 using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using XDB.Common.Attributes;
-using XDB.Common.Enums;
 using XDB.Common.Types;
 
 namespace XDB.Modules
@@ -155,7 +150,6 @@ namespace XDB.Modules
             embed.AddField(x =>
             {
                 x.Name = "Owner:";
-                //x.Value = owner.Mention;
                 x.Value = guild.Owner.Mention;
                 x.IsInline = true;
             });
@@ -204,73 +198,6 @@ namespace XDB.Modules
             await ReplyAsync("", false, embed.Build());
         }
         
-        [Command("info")]
-        [Remarks("Display's the bots information and statistics.")]
-        public async Task Info()
-        {
-            var application = await Context.Client.GetApplicationInfoAsync();
-            var auth = new EmbedAuthorBuilder()
-            {
-                Name = Context.Client.CurrentUser.Username,
-                IconUrl = Context.Client.CurrentUser.AvatarUrl
-            };
-            var embed = new EmbedBuilder()
-            {
-                Color = new Color(29, 140, 209),
-                Author = auth
-            };
-            embed.AddField(x =>
-            {
-                x.Name = "Author:";
-                x.Value = $"{application.Owner.Username} (**ID:** `{application.Owner.Id}`)";
-                x.IsInline = false;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Version:";
-                x.Value = $"`XDB v{Strings.ReleaseVersion}`";
-                x.IsInline = false;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Library:";
-                x.Value = $"Discord.Net (`{DiscordConfig.Version}`)";
-                x.IsInline = false;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Runtime:";
-                x.Value = $"{RuntimeInformation.FrameworkDescription} ({RuntimeInformation.OSArchitecture})";
-                x.IsInline = false;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Host OS:";
-                x.Value = RuntimeInformation.OSDescription;
-                x.IsInline = false;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Uptime:";
-                x.Value = $"`{GetUptime()}`";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Heap Size:";
-                x.Value = $"`{GetHeapSize()}MB`";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Guilds/Channels/Users:";
-                x.Value = $"{(Context.Client as DiscordSocketClient).Guilds.Count} / {(Context.Client as DiscordSocketClient).Guilds.Sum(g => g.Channels.Count)} / {(Context.Client as DiscordSocketClient).Guilds.Sum(g => g.Users.Count)}";
-                x.IsInline = false;
-            });
-
-            await ReplyAsync("", false, embed.Build());
-        }
-
         private string GetUserGame(CommandContext ctx)
         {
             var playing = ctx.User.Game;
@@ -290,9 +217,5 @@ namespace XDB.Modules
             }
             return $"`{playing}`";
         }
-
-        private static string GetUptime()
-            => (DateTime.Now - Process.GetCurrentProcess().StartTime).ToString(@"dd\.hh\:mm\:ss");
-        private static string GetHeapSize() => Math.Round(GC.GetTotalMemory(true) / (1024.0 * 1024.0), 2).ToString();
     }
 }
--- src/XDB/Modules/Self.cs
+++ src/XDB/Modules/Self.cs
@@ -1,10 +1,16 @@
 ﻿using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using XDB.Common.Attributes;
 using XDB.Common.Enums;
+using XDB.Common.Types;
 
 namespace XDB.Modules
 {
@@ -92,5 +98,77 @@ namespace XDB.Modules
             var current = await Context.Guild.GetCurrentUserAsync();
             await current.Guild.LeaveAsync();
         }
+
+        [Command("info")]
+        [Remarks("Display's the bots information and statistics.")]
+        public async Task Info()
+        {
+            var application = await Context.Client.GetApplicationInfoAsync();
+            var auth = new EmbedAuthorBuilder()
+            {
+                Name = Context.Client.CurrentUser.Username,
+                IconUrl = Context.Client.CurrentUser.AvatarUrl
+            };
+            var embed = new EmbedBuilder()
+            {
+                Color = new Color(29, 140, 209),
+                Author = auth
+            };
+            embed.AddField(x =>
+            {
+                x.Name = "Author:";
+                x.Value = $"{application.Owner.Username} (**ID:** `{application.Owner.Id}`)";
+                x.IsInline = false;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Version:";
+                x.Value = $"`XDB v{Strings.ReleaseVersion}`";
+                x.IsInline = false;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Library:";
+                x.Value = $"Discord.Net (`{DiscordConfig.Version}`)";
+                x.IsInline = false;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Runtime:";
+                x.Value = $"{RuntimeInformation.FrameworkDescription} ({RuntimeInformation.OSArchitecture})";
+                x.IsInline = false;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Host OS:";
+                x.Value = RuntimeInformation.OSDescription;
+                x.IsInline = false;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Uptime:";
+                x.Value = $"`{GetUptime()}`";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Heap Size:";
+                x.Value = $"`{GetHeapSize()}MB`";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Guilds/Channels/Users:";
+                x.Value = $"{(Context.Client as DiscordSocketClient).Guilds.Count} / {(Context.Client as DiscordSocketClient).Guilds.Sum(g => g.Channels.Count)} / {(Context.Client as DiscordSocketClient).Guilds.Sum(g => g.Users.Count)}";
+                x.IsInline = false;
+            });
+
+            await ReplyAsync("", false, embed.Build());
+        }
+
+
+        private static string GetUptime()
+            => (DateTime.Now - Process.GetCurrentProcess().StartTime).ToString(@"dd\.hh\:mm\:ss");
+        private static string GetHeapSize() => Math.Round(GC.GetTotalMemory(true) / (1024.0 * 1024.0), 2).ToString();
     }
 }
