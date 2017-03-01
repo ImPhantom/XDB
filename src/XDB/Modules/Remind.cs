75f1e84dc79049394aa7f136497b12e837eb323e
ImPhantom <danieljcannon@live.com>
2017-03-01 03:43:58 -0500

    Very rough reminder system.

--- src/XDB/Modules/Remind.cs
+++ src/XDB/Modules/Remind.cs
@@ -1,34 +1,25 @@
 ﻿using Discord.Commands;
 using System.Threading.Tasks;
-using System.Threading;
 using System;
+using XDB.Services;
 
 namespace XDB.Modules
 {
     public class Remind : ModuleBase
     {
+        // UNDONE
+        // SERIOUSLY UNDONE
         [Command("remind")]
         [Remarks("Sets a reminder for the calling user.")]
         [RequireContext(ContextType.Guild)]
-        public async Task RemindMe(int ms)
+        public async Task RemindMe(string time, [Remainder] string reminder)
         {
-            var timer = new Timer(async _ =>
-            {
-                await ReplyAsync("Timer ran out.");
-            }, null, ms, Timeout.Infinite);
-            await ReplyAsync("Timer made?");
+            var ms = Convert.ToInt32(ToMS(time));
+            var serv = new TimerService(ms, reminder, Context.User.Id, Context.Channel.Id);
+            await ReplyAsync($":alarm_clock: Okay, I will remind you in {time}.");
         }
 
-        [Command("time")]
-        [Remarks("t")]
-        [RequireContext(ContextType.Guild)]
-        public async Task Time(string time)
-        {
-            var timespan = ToTimeSpan(time);
-            await ReplyAsync($"{timespan.ToString()}");
-        }
-
-        private static TimeSpan ToTimeSpan(string time)
+        private static double ToMS(string time)
         {
             var length = time.Length - 1;
             var value = time.Substring(0, length);
@@ -36,10 +27,11 @@ namespace XDB.Modules
 
             switch (type)
             {
-                case "d": return TimeSpan.FromDays(double.Parse(value));
-                case "m": return TimeSpan.FromMinutes(double.Parse(value));
-                case "h": return TimeSpan.FromHours(double.Parse(value));
-                default: return TimeSpan.FromHours(double.Parse(value));
+                case "d": return TimeSpan.FromDays(double.Parse(value)).TotalMilliseconds;
+                case "m": return TimeSpan.FromMinutes(double.Parse(value)).TotalMilliseconds;
+                case "h": return TimeSpan.FromHours(double.Parse(value)).TotalMilliseconds;
+                case "s": return TimeSpan.FromSeconds(double.Parse(value)).TotalMilliseconds;
+                default: return TimeSpan.FromHours(double.Parse(value)).TotalMilliseconds;
             }
         }
     }
---
+++ src/XDB/Services/TimerService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using Discord;
+
+namespace XDB.Services
+{
+    public class TimerService
+    {
+        private readonly Timer _timer;
+
+        public TimerService(int time, string reminder, ulong user, ulong channel)
+        {
+            var client = Program.client;
+            _timer = new Timer(_ =>
+            {
+                var c = client.GetChannel(channel) as ITextChannel;
+                var u = client.GetUser(user) as IUser;
+                c.SendMessageAsync($":mega: {u.Mention} Timer is up! You need to do: `{reminder}`");
+            }, null, time, Timeout.Infinite);
+        }
+
+        public void Stop()
+        {
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+    }
+}
