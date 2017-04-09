6c115e2a33bd18473f2b3929b8879181a1cb3f8f
ImPhantom <danieljcannon@live.com>
2017-04-09 02:42:22 -0400

    Improved guild/user info commands

--- XDB/Modules/Chat.cs
+++ XDB/Modules/Chat.cs
@@ -14,106 +14,26 @@ namespace XDB.Modules
         [Command("8ball"), Summary("Asks the magic 8 ball a question.")]
         public async Task EightBall([Remainder] string question)
         {
-            Random rand = new Random();
-            var response = Strings.EightBallResponses[rand.Next(Strings.EightBallResponses.Length)];
+            Random random = new Random();
+            var response = Strings.EightBallResponses[random.Next(Strings.EightBallResponses.Length)];
             await ReplyAsync($":8ball: {response}");
         }
 
         [Command("userinfo"), Summary("Displays your user information.")]
         public async Task UserInfo()
-        {
-            var author = new EmbedAuthorBuilder().WithName(Context.User.Username).WithIconUrl(Context.User.GetAvatarUrl());
-            var embed = new EmbedBuilder().WithColor(new Color(29, 140, 209)).WithAuthor(author);
-            embed.AddField(x =>
-            {
-                x.Name = "Username:";
-                x.Value = $"{Context.User.Username}";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Discriminator:";
-                x.Value = $"{Context.User.Discriminator}";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "ID:";
-                x.Value = $"`{Context.User.Id}`";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Created:";
-                x.Value = $"{Context.User.CreatedAt.Month}/{Context.User.CreatedAt.Day}/{Context.User.CreatedAt.Year}";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Status:";
-                x.Value = $"{Context.User.Status}";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Game:";
-                x.Value = $"{GetUserGame(Context.User as SocketUser)}";
-                x.IsInline = true;
-            });
-            await ReplyAsync("", false, embed.Build());
-        }
+            => await GetUserInfo(Context, Context.User as SocketGuildUser);
 
         [Command("userinfo"), Summary("Displays a specified users information.")]
         [Name("userinfo `<user>`")]
-        public async Task UserInfo(SocketUser user)
-        {
-            var author = new EmbedAuthorBuilder().WithName(user.Username).WithIconUrl(user.GetAvatarUrl());
-            var embed = new EmbedBuilder().WithColor(new Color(29, 140, 209)).WithAuthor(author);
-            embed.AddField(x =>
-            {
-                x.Name = "Username:";
-                x.Value = $"{user.Username}";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Discriminator:";
-                x.Value = $"{user.Discriminator}";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "ID:";
-                x.Value = $"`{user.Id}`";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Created:";
-                x.Value = $"{user.CreatedAt.Month}/{user.CreatedAt.Day}/{user.CreatedAt.Year}";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Status:";
-                x.Value = $"{user.Status}";
-                x.IsInline = true;
-            });
-            embed.AddField(x =>
-            {
-                x.Name = "Game:";
-                x.Value = $"{GetUserGame(user)}";
-                x.IsInline = true;
-            });
-            await ReplyAsync("", false, embed.Build());
-        }
+        public async Task UserInfo(SocketGuildUser user)
+            => await GetUserInfo(Context, user);
 
-        [Command("serverinfo"), Summary("Display's the current guild's information.")]
-        public async Task ServerInfo()
+        [Command("guild"), Alias("server"), Summary("Display's the current guild's information.")]
+        public async Task GuildInfo()
         {
             var guild = Context.Guild as SocketGuild;
-            var footer = new EmbedFooterBuilder().WithText($"Server ID: {guild.Id}");
-            var embed = new EmbedBuilder().WithColor(new Color(29, 140, 209)).WithThumbnailUrl(guild.IconUrl).WithFooter(footer);
+            var author = new EmbedAuthorBuilder().WithIconUrl(guild.IconUrl).WithName(guild.Name);
+            var embed = new EmbedBuilder().WithColor(new Color(29, 140, 209)).WithAuthor(author).WithCurrentTimestamp();
             embed.AddField(x =>
             {
                 x.Name = "Owner:";
@@ -122,8 +42,8 @@ namespace XDB.Modules
             });
             embed.AddField(x =>
             {
-                x.Name = "Server Name:";
-                x.Value = $"{guild.Name}";
+                x.Name = "Guild ID:";
+                x.Value = $"`{guild.Id}`";
                 x.IsInline = true;
             });
             embed.AddField(x =>
@@ -165,12 +85,59 @@ namespace XDB.Modules
             await ReplyAsync("", false, embed.Build());
         }
 
-        private string GetUserGame(SocketUser user)
+
+
+        private async Task GetUserInfo(CommandContext context, SocketGuildUser user)
         {
-            if (!user.Game.HasValue)
-                return $"`N/A`";
-            else
-                return $"`{user.Game}`";
+            var author = new EmbedAuthorBuilder().WithName(Context.User.Username).WithIconUrl(Context.User.GetAvatarUrl());
+            var embed = new EmbedBuilder().WithColor(new Color(29, 140, 209)).WithAuthor(author).WithCurrentTimestamp();
+
+            embed.AddField(x =>
+            {
+                x.Name = "Username:";
+                x.Value = $"`{user.Username}#{user.Discriminator}`";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "ID:";
+                x.Value = $"`{user.Id}`";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Created:";
+                x.Value = $"{user.CreatedAt.Month}/{user.CreatedAt.Day}/{user.CreatedAt.Year}";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Joined:";
+                x.Value = $"{user.JoinedAt.Value.Month}/{user.JoinedAt.Value.Day}/{user.JoinedAt.Value.Year}";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Status:";
+                x.Value = $"{user.Status}";
+                x.IsInline = true;
+            });
+            embed.AddField(x =>
+            {
+                x.Name = "Game:";
+                x.Value = $"{Strings.GetUserGame(user)}";
+                x.IsInline = true;
+            });
+            if (user.VoiceChannel != null)
+            {
+                embed.AddField(x =>
+                {
+                    x.Name = "Voice State: ";
+                    x.Value = $"{Strings.GetVoiceState(user)}";
+                    x.IsInline = true;
+                });
+            }
+            await ReplyAsync("", false, embed.Build());
         }
     }
 }
