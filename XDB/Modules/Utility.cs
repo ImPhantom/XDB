8c116b020eca6aee0f674d782227129a726eab3e
ImPhantom <danieljcannon@live.com>
2017-05-13 15:04:21 -0400

    Moved from Google Json parsing to Google.Apis.

--- XDB/Modules/Utility.cs
+++ XDB/Modules/Utility.cs
@@ -1,13 +1,21 @@
 ﻿using Discord;
 using Discord.Commands;
+using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
+using Google.Apis.Urlshortener.v1;
+using Google.Apis.Services;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using XDB.Common.Types;
+using Google.Apis.Urlshortener.v1.Data;
 
 namespace XDB.Modules
 {
@@ -15,6 +23,26 @@ namespace XDB.Modules
     [RequireContext(ContextType.Guild)]
     public class Utility : ModuleBase<SocketCommandContext>
     {
+        private CustomsearchService _search;
+        private UrlshortenerService _shortener;
+
+        protected override void BeforeExecute()
+        {
+            if (string.IsNullOrEmpty(Config.Load().GoogleKey))
+                throw new InvalidOperationException("Google API Key is not set in config!");
+
+            _search = new CustomsearchService(new BaseClientService.Initializer()
+            {
+                ApiKey = Config.Load().GoogleKey,
+                MaxUrlLength = 256
+            });
+
+            _shortener = new UrlshortenerService(new BaseClientService.Initializer()
+            {
+                ApiKey = Config.Load().GoogleKey
+            });
+        }
+
         [Command("shorten"), Summary("Shortens a link.")]
         public async Task Shorten([Remainder] string url)
         {
@@ -23,31 +51,17 @@ namespace XDB.Modules
                 await ReplyAsync(":heavy_multiplication_x:  Please provide a valid link.");
                 return;
             }
-            var request = (HttpWebRequest)WebRequest.Create($"https://www.googleapis.com/urlshortener/v1/url?key={Config.Load().GoogleKey}");
-            try
-            {
-                request.ContentType = "application/json";
-                request.Method = "POST";
+            var response = _shortener.Url.Insert(new Url { LongUrl = url }).ExecuteAsync();
+            await Context.Message.DeleteAsync();
+            await ReplyAsync($":heavy_check_mark:  Shortened: {response.Result.Id}");
+        }
 
-                var requestStream = await request.GetRequestStreamAsync();
-                using (var writer = new StreamWriter(requestStream))
-                {
-                    string json = $"{{\"longUrl\": \" {url} \"}}";
-                    writer.Write(json);
-                }
-
-                var response = (HttpWebResponse)await request.GetResponseAsync();
-                using (var reader = new StreamReader(response.GetResponseStream()))
-                {
-                    var shorturl = Regex.Match(reader.ReadToEnd(), @"""id"": ?""(?<id>.+)""").Groups["id"].Value;
-                    await ReplyAsync($":heavy_check_mark:  Shortened: {shorturl}");
-                }
-                await Context.Message.DeleteAsync();
-            } catch (Exception e)
-            {
-                await ReplyAsync($"**Error:** Shortener is down.");
-                Console.WriteLine(e.Message);
-            }
+        [Command("google"), Alias("g"), Summary("Returns a search query from google.")]
+        public async Task Google([Remainder] string query)
+        {
+            var results = (await SearchGoogleAsync(query)).ToArray();
+            var embed = new EmbedBuilder().WithColor(new Color(16, 178, 232)).WithTitle($"Search results for: {query}").WithDescription($"{results[0].Link}\n\nSee also:\n{results[1].Link}\n{results[2].Link}");
+            await ReplyAsync("", embed: embed);
         }
 
         [Command("urban"), Summary("Retrieves a definition from UrbanDictionary.")]
@@ -79,6 +93,20 @@ namespace XDB.Modules
                     await ReplyAsync(":heavy_multiplication_x:  Could not find a definition for that word!");
                 }
             }
+        }
+
+        private async Task<IEnumerable<Result>> SearchGoogleAsync(string query, Uri site = null)
+        {
+            var request = _search.Cse.List(query);
+            request.Cx = "010537608855081241185:b3t-ivrqjwg";
+            request.Num = 3;
+            request.Safe = CseResource.ListRequest.SafeEnum.Medium;
+
+            if (site != null)
+                request.SiteSearch = site.ToString();
+
+            var result = await request.ExecuteAsync();
+            return result.Items;
         }
     }
 }
