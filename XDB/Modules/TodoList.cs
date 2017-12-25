9c37ee98b0e7522a2036f53b5183d948c751b76c
ImPhantom <danieljcannon@live.com>
2017-12-24 22:51:11 -0500

    Removed channel todo lists

--- XDB/Modules/TodoList.cs
+++ XDB/Modules/TodoList.cs
@@ -13,7 +13,7 @@ namespace XDB.Modules
         [Command, Summary("Displays your todo list.")]
         public async Task ViewTodo()
         {
-            var chList = _lists.FetchListContent(Context.User.Id, Context.Channel.Id);
+            var chList = _lists.FetchListItems(Context.User.Id);
             if (!chList.StartsWith(":"))
                 await ReplyAsync("", embed: Xeno.ErrorEmbed(chList));
             else
@@ -23,7 +23,7 @@ namespace XDB.Modules
         [Command("add"), Summary("Adds item to you todo list.")]
         public async Task AddTodo([Remainder] string listitem)
         {
-            var result = await _lists.AddListItemAsync(Context.User.Id, Context.Guild.Id, Context.Channel.Id, listitem);
+            var result = await _lists.AddTodoListItemAsync(Context.User.Id, listitem);
             if (!result.StartsWith(":"))
                 await ReplyAsync("", embed: Xeno.ErrorEmbed(result));
             else
@@ -33,7 +33,7 @@ namespace XDB.Modules
         [Command("del"), Alias("rem","remove","delete"), Summary("Removes an item from your todo list by index.")]
         public async Task DelTodo(int index)
         {
-            var result = await _lists.RemoveListItemAsync(Context.User.Id, Context.Guild.Id, Context.Channel.Id, index);
+            var result = await _lists.RemoveTodoListItemAsync(Context.User.Id, index);
             if (!result.StartsWith(":"))
                 await ReplyAsync("", embed: Xeno.ErrorEmbed(result));
             else
@@ -43,7 +43,7 @@ namespace XDB.Modules
         [Command("clear"), Summary("Clears your todo list.")]
         public async Task ClearTodo()
         {
-            var result = await _lists.ClearListAsync(Context.User.Id, Context.Channel.Id);
+            var result = await _lists.ClearTodoListAsync(Context.User.Id);
             if (!result.StartsWith(":"))
                 await ReplyAsync("", embed: Xeno.ErrorEmbed(result));
             else
@@ -53,7 +53,7 @@ namespace XDB.Modules
         [Command("edit"), Summary("Edits an item on your todo list.")]
         public async Task EditTodo(int index, [Remainder] string edit)
         {
-            var result = await _lists.EditListItemAsync(Context.User.Id, Context.Guild.Id, Context.Channel.Id, index, edit);
+            var result = await _lists.EditTodoListItemAsync(Context.User.Id, index, edit);
             if (!result.StartsWith(":"))
                 await ReplyAsync("", embed: Xeno.ErrorEmbed(result));
             else
@@ -61,27 +61,6 @@ namespace XDB.Modules
         }
 
         public TodoList(ListService lists)
-        {
-            _lists = lists;
-        }
-    }
-
-    public class DevChannels : ModuleBase
-    {
-        private ListService _lists;
-
-        [Command("dev"), Summary("Marks a text channel as a development channel")]
-        [RequireOwner]
-        public async Task MarkDev()
-        {
-            var result = await _lists.CreateChannelListAsync(Context.Guild.Id, Context.Channel.Id);
-            if (!result.StartsWith(":"))
-                await ReplyAsync("", embed: Xeno.ErrorEmbed(result));
-            else
-                await ReplyAsync(result);
-        }
-
-        public DevChannels(ListService lists)
         {
             _lists = lists;
         }
--- XDB/Services/ListService.cs
+++ XDB/Services/ListService.cs
@@ -9,68 +9,13 @@ using System.Threading.Tasks;
 using XDB.Common.Models;
 
 namespace XDB.Services
-{
-    //
-    //      TODO: Permissions for channel lists (as of now, anyone in the channel can edit/remove/clear/add list items)
-    //      
-
+{    
     public class ListService
     {
         private DiscordSocketClient _client;
 
-        public List<UserTodo> FetchLists()
+        private List<UserTodo> FetchLists()
             => JsonConvert.DeserializeObject<List<UserTodo>>(File.ReadAllText(Xeno.TodoPath));
-
-        public List<ChannelTodo> FetchChannelLists()
-            => JsonConvert.DeserializeObject<List<ChannelTodo>>(File.ReadAllText(Xeno.DevListPath));
-
-        #region Fetching Lists
-
-        // To fetch all lists depending on channel
-        public string FetchListContent(ulong userId, ulong channelId)
-        {
-            var _todo = FetchLists();
-            var _devc = FetchChannelLists();
-            if (_devc.Any(x => x.ChannelId == channelId))
-                return FetchChannelListItems(channelId);
-            else
-                return FetchListItems(userId);
-            // Check for null in ~todo then it failed to get either devchannel list or todo list
-        }
-
-        // For dev channel lists
-
-        public string FetchChannelListItems(ulong channelId)
-        {
-            try
-            {
-                var _in = FetchChannelLists();
-                if (_in.Any(x => x.ChannelId == channelId))
-                {
-                    var list = _in.First(x => x.ChannelId == channelId);
-                    if (!list.Items.Any())
-                        return ":eight_pointed_black_star:  This todo list is empty.";
-
-                    var ol = new StringBuilder();
-                    int count = 1;
-                    foreach (var item in list.Items)
-                    {
-                        ol.AppendLine($"{count}. {item}");
-                        count++;
-                    }
-                    return $":small_blue_diamond: **Channel Todo List:**```{ol.ToString()}```";
-                }
-                else
-                    return ":eight_pointed_black_star:  This todo list is empty.";
-            }
-            catch (Exception ex)
-            {
-                BetterConsole.LogError("List Service", $"{ex.Message}{ex.StackTrace}");
-                return $"{ex.Message}";
-            }
-        }
-
-        // For Todo Lists
 
         public string FetchListItems(ulong userId)
         {
@@ -101,20 +46,8 @@ namespace XDB.Services
                 return $"{ex.Message}";
             }
         }
-        #endregion
 
-        #region Adding List Items
-        public async Task<string> AddListItemAsync(ulong userId, ulong guildId, ulong channelId, string item)
-        {
-            var _todo = FetchLists();
-            var _dev = FetchChannelLists();
-            if (_dev.Any(x => x.ChannelId == channelId))
-                return await AddChannelListItemAsync(guildId, channelId, item);
-            else
-                return await AddTodoListItemAsync(userId, item);
-        }
-
-        private async Task<string> AddTodoListItemAsync(ulong userId, string item)
+        public async Task<string> AddTodoListItemAsync(ulong userId, string item)
         {
             try
             {
@@ -143,37 +76,7 @@ namespace XDB.Services
             }
         }
 
-        private async Task<string> AddChannelListItemAsync(ulong guildId, ulong channelId, string item)
-        {
-            try
-            {
-                var _in = FetchChannelLists();
-                _in.First(x => x.ChannelId == channelId).Items.Add(item);
-                var _out = JsonConvert.SerializeObject(_in);
-                await Save(Xeno.DevListPath, _out);
-                await UpdateChannelTopic(_in.First(x => x.ChannelId == channelId));
-                return FetchChannelListItems(_in.First(x => x.ChannelId == channelId).ChannelId);
-            }
-            catch (Exception ex)
-            {
-                BetterConsole.LogError("List Service", $"{ex.Message}{ex.StackTrace}");
-                return $"{ex.Message}";
-            }
-        }
-        #endregion
-
-        #region Edit List Items
-        public async Task<string> EditListItemAsync(ulong userId, ulong guildId, ulong channelId, int index, string content)
-        {
-            var _todo = FetchLists();
-            var _dev = FetchChannelLists();
-            if (_dev.Any(x => x.ChannelId == channelId))
-                return await EditChannelListItemAsync(channelId, index, content);
-            else
-                return await EditTodoListItemAsync(userId, index, content);
-        }
-
-        private async Task<string> EditTodoListItemAsync(ulong userId, int index, string content)
+        public async Task<string> EditTodoListItemAsync(ulong userId, int index, string content)
         {
             try
             {
@@ -203,51 +106,7 @@ namespace XDB.Services
             }
         }
 
-        private async Task<string> EditChannelListItemAsync(ulong channelId, int index, string content)
-        {
-            try
-            {
-                var _in = FetchChannelLists();
-                if (_in.Any(x => x.ChannelId == channelId))
-                {
-                    if (!_in.First(x => x.ChannelId == channelId).Items.Any())
-                        return ":eight_pointed_black_star:  Your todo list is empty.";
-                    index--;
-                    if (index < _in.First(x => x.ChannelId == channelId).Items.Count)
-                    {
-                        _in.First(x => x.ChannelId == channelId).Items[index] = content;
-                        var _out = JsonConvert.SerializeObject(_in);
-                        await Save(Xeno.DevListPath, _out);
-                        await UpdateChannelTopic(_in.First(x => x.ChannelId == channelId));
-                        return FetchChannelListItems(_in.First(x => x.ChannelId == channelId).ChannelId);
-                    }
-                    else
-                        return ":heavy_multiplication_x:  There is no list item for that index.";
-                }
-                else
-                    return ":eight_pointed_black_star:  The todo list is empty.";
-            }
-            catch (Exception ex)
-            {
-                BetterConsole.LogError("List Service", $"{ex.Message}{ex.StackTrace}");
-                return $"{ex.Message}";
-            }
-        }
-
-        #endregion
-
-        #region Remove List Items
-        public async Task<string> RemoveListItemAsync(ulong userId, ulong guildId, ulong channelId, int index)
-        {
-            var _todo = FetchLists();
-            var _dev = FetchChannelLists();
-            if (_dev.Any(x => x.ChannelId == channelId))
-                return await RemoveChannelListItemAsync(channelId, index);
-            else
-                return await RemoveTodoListItemAsync(userId, index);
-        }
-
-        private async Task<string> RemoveTodoListItemAsync(ulong userId, int index)
+        public async Task<string> RemoveTodoListItemAsync(ulong userId, int index)
         {
             try
             {
@@ -277,50 +136,7 @@ namespace XDB.Services
             }
         }
 
-        private async Task<string> RemoveChannelListItemAsync(ulong channelId, int index)
-        {
-            try
-            {
-                var _in = FetchChannelLists();
-                if (_in.Any(x => x.ChannelId == channelId))
-                {
-                    if (!_in.First(x => x.ChannelId == channelId).Items.Any())
-                        return ":eight_pointed_black_star:  The todo list is empty.";
-                    index--;
-                    if (index < _in.First(x => x.ChannelId == channelId).Items.Count)
-                    {
-                        _in.First(x => x.ChannelId == channelId).Items.RemoveAt(index);
-                        var _out = JsonConvert.SerializeObject(_in);
-                        await Save(Xeno.DevListPath, _out);
-                        await UpdateChannelTopic(_in.First(x => x.ChannelId == channelId));
-                        return FetchChannelListItems(_in.First(x => x.ChannelId == channelId).ChannelId);
-                    }
-                    else
-                        return ":heavy_multiplication_x:  There is no list item for that index.";
-                }
-                else
-                    return ":eight_pointed_black_star:  The todo list is empty.";
-            }
-            catch (Exception ex)
-            {
-                BetterConsole.LogError("List Service", $"{ex.Message}{ex.StackTrace}");
-                return $"{ex.Message}";
-            }
-        }
-        #endregion
-
-        #region Clear List
-        public async Task<string> ClearListAsync(ulong userId, ulong channelId)
-        {
-            var _todo = FetchLists();
-            var _dev = FetchChannelLists();
-            if (_dev.Any(x => x.ChannelId == channelId))
-                return await ClearChannelListAsync(channelId);
-            else
-                return await ClearTodoListAsync(userId);
-        }
-
-        private async Task<string> ClearTodoListAsync(ulong userId)
+        public async Task<string> ClearTodoListAsync(ulong userId)
         {
             try
             {
@@ -347,77 +163,6 @@ namespace XDB.Services
             }
         }
 
-        private async Task<string> ClearChannelListAsync(ulong channelId)
-        {
-            try
-            {
-                var _in = FetchChannelLists();
-                if (_in.Any(x => x.ChannelId == channelId))
-                {
-                    if (!_in.First(x => x.ChannelId == channelId).Items.Any())
-                        return ":eight_pointed_black_star:  This todo list is already empty.";
-                    else
-                    {
-                        _in.First(x => x.ChannelId == channelId).Items.Clear();
-                        var _out = JsonConvert.SerializeObject(_in);
-                        await Save(Xeno.DevListPath, _out);
-                        await UpdateChannelTopic(_in.First(x => x.ChannelId == channelId));
-                        return ":heavy_check_mark: Successfully cleared the todo list.";
-                    }
-                }
-                else
-                    return ":eight_pointed_black_star:  This todo list is already empty.";
-            }
-            catch (Exception ex)
-            {
-                BetterConsole.LogError("List Service", $"{ex.Message}{ex.StackTrace}");
-                return $"{ex.Message}";
-            }
-        }
-        #endregion
-
-        #region Channel Topic Handling
-        private async Task UpdateChannelTopic(ChannelTodo todo)
-        {
-            var guild = _client.GetGuild(todo.GuildId);
-            var channel = guild.GetTextChannel(todo.ChannelId);
-            var todoList = new StringBuilder();
-            todoList.Append(":small_blue_diamond: Channel Todo List: (click here)\r\n------------------------------------------------------\r\n");
-            int count = 1;
-            foreach(var item in todo.Items)
-            {
-                todoList.AppendLine($"{count}.) {item}\r\n");
-                count++;
-            }
-            await channel.ModifyAsync(x => x.Topic = todoList.ToString());
-        }
-        #endregion
-
-        public async Task<string> CreateChannelListAsync(ulong guildId, ulong channelId)
-        {
-            try
-            {
-                var _in = FetchChannelLists();
-                if (_in.Any(x => x.ChannelId == channelId))
-                    return ":eight_pointed_black_star:  This channel is already a development channel.";
-                var list = new ChannelTodo()
-                {
-                    GuildId = guildId,
-                    ChannelId = channelId,
-                    Items = new List<string> { }
-                };
-                _in.Add(list);
-                var _out = JsonConvert.SerializeObject(_in);
-                await Save(Xeno.DevListPath, _out);
-                await UpdateChannelTopic(list);
-                return ":heavy_check_mark: Successfully marked this as a dev channel.";
-            } catch (Exception ex)
-            {
-                BetterConsole.LogError("List Service", $"{ex.Message}{ex.StackTrace}");
-                return $"{ex.Message}";
-            }
-        }
-
         private async Task Save(string path, string json)
         {
             using (var stream = new FileStream(path, FileMode.Truncate))
@@ -436,13 +181,6 @@ namespace XDB.Services
                 List<UserTodo> lists = new List<UserTodo>();
                 using (var file = new FileStream(Xeno.TodoPath, FileMode.Create)) { }
                 File.WriteAllText(Xeno.TodoPath, JsonConvert.SerializeObject(lists));
-            }
-
-            if (!File.Exists(Xeno.DevListPath))
-            {
-                List<ChannelTodo> chLists = new List<ChannelTodo>();
-                using (var file = new FileStream(Xeno.TodoPath, FileMode.Create)) { }
-                File.WriteAllText(Xeno.DevListPath, JsonConvert.SerializeObject(chLists));
             }
         }
 
