11a6494008c2a32a386b76f861357584f3c76e82
ImPhantom <danieljcannon@live.com>
2018-02-01 23:25:04 -0500

    Touched up the request system

--- XDB/Modules/Request.cs
+++ XDB/Modules/Request.cs
@@ -10,27 +10,31 @@ using System.Linq;
 using Discord;
 using Humanizer;
 using XDB.Common.Attributes;
+using Discord.WebSocket;
 
 namespace XDB.Modules
 {
     public class Request : ModuleBase
     {
         [Command("request")]
-        [RequireStaff]
+        [RequireContext(ContextType.Guild)]
+        [RequirePermission(Permission.Staff)]
         public async Task RequestBan(string steamId, TimeSpan length, [Remainder] string reason)
         {
             var valid = await ValidateSteamId(steamId);
             if (valid != null)
             {
                 await Context.Message.DeleteAsync();
+                var rby = GetDisplayName(Context.User as SocketGuildUser);
+                var full = $"{reason} (Banned on behalf of: {rby})";
                 var id32 = SteamUtil.ToSteam32(steamId);
                 var author = new EmbedAuthorBuilder().WithName(valid.PersonaName).WithIconUrl(valid.MediumSizeAvatarUrl);
                 var embed = new EmbedBuilder().WithAuthor(author).WithColor(Xeno.RandomColor());
                 embed.AddField("SteamIDs:", $"{id32}  ( {valid.Steam64Id} )", false);
-                embed.AddField("Requested By:", $"{Context.User.Username}#{Context.User.Discriminator}", true);
-                embed.AddField("Duration:", $"{length.Humanize()}", true);
-                embed.AddField("Reason:", $"{reason} (Banned on behalf of: {Context.User.Username})");
-                embed.AddField("Command:", $"`ulx banid {id32} {length.TotalMinutes} \"{reason}\"`");
+                embed.AddField("Requested By:", $"{rby}#{Context.User.Discriminator}", true);
+                embed.AddField("Duration:", $"{Humanize(length)}", true);
+                embed.AddField("Reason:", full);
+                embed.AddField("Command:", $"`ulx banid {id32} {length.TotalMinutes} \"{full}\"`");
                 await ReplyAsync("<@!169591360695959553>", embed: embed.Build());
             } else
             {
@@ -58,6 +62,22 @@ namespace XDB.Modules
                 else
                     return null;
             }
+        }
+
+        private string Humanize(TimeSpan time)
+        {
+            if (time.TotalMinutes == 0)
+                return "Perma";
+            else
+                return time.Humanize();
+        }
+
+        private string GetDisplayName(SocketGuildUser user)
+        {
+            if (user.Nickname != null)
+                return user.Nickname;
+            else
+                return user.Username;
         }
     }
 }
